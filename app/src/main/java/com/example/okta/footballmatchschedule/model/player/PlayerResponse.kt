201f6b725a62a6bad7204210f0773dc0b29ad3da
Okta <okta.p.nainggolan@gmail.com>
2018-11-23 17:35:55 +0700

    pre submission5 ver2

---
+++ app/src/main/java/com/example/okta/footballmatchschedule/model/player/PlayerResponse.kt
@@ -0,0 +1,11 @@
+package com.example.okta.footballmatchschedule.model.player
+
+import javax.annotation.Generated
+import com.google.gson.annotations.SerializedName
+
+@Generated("com.robohorse.robopojogenerator")
+data class PlayerResponse(
+
+	@field:SerializedName("player")
+	val player: List<PlayerItem?>? = null
+)
